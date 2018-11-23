cb60034fbdc4846c401882d2d9a15f0980a864b9
Alexander <alexander.botello@g.austincc.edu>
2018-11-23 16:10:02 -0600

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
+import os
+import re
+
 from setuptools import setup
+
+def get_version(module):
+    init_py = open(os.path.join(module. "__init__.py")).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
 setup(
     name="minimize",
-    version="0.1",
-    py_modules=["cli", "minimize"],
-    install_requires=["Click", "Pillow", "mypy"],
+    version=get_version("minimize")
+    py_modules=["minimize"],
+    install_requires=["Click", "Pillow"],
     entry_points="""
         [console_scripts]
         minimize=cli:run
