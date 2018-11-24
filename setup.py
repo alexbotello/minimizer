fe34040494ff424d70dd34b7bf722df7a09f97e4
Alexander <alexander.botello@g.austincc.edu>
2018-11-23 22:27:13 -0600

    version 0.3

--- setup.py
+++ setup.py
@@ -1,24 +1,20 @@
-import os
-import re
+import setuptools
 
-from setuptools import setup
-
-def get_version(module):
-    init_py = open(os.path.join(module, "__init__.py")).read()
-    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
 def get_long_description():
-    return open('README.md', 'r', encoding="utf8").read()
+    return open("README.md", "r", encoding="utf8").read()
 
-setup(
+
+setuptools.setup(
     name="minimizer",
-    version=get_version("minimizer"),
+    version="0.3",
     url="https://github.com/alexbotello/minimizer",
     license="MIT",
     description="A command line tool to shrink image(s) in a directory to a specified size",
     long_description=get_long_description(),
     author="Alexander Botello",
     author_email="alexander.botello@g.austincc.edu",
+    packages=setuptools.find_packages(),
     py_modules=["minimizer"],
     install_requires=["Click", "Pillow"],
     entry_points="""
